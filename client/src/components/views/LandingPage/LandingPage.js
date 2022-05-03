ea588dfbfd4c0162af9cea0e7e57088d4fea8955
juserh <rosapark07@gmail.com>
2022-05-03 19:46:17 +0900

    api 데이터 useEffect로 받아오기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY } from "../../Config";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  }, []);
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/*Main Image */}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr />
+        {/*Movie Grid Cards */}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
